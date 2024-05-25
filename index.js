8187c60b8372b097f96cdf8547f3a9c02e04568a
Raseeca Kashelkar <142300062+ItsMeRaseeca@users.noreply.github.com>
2024-05-25 19:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ $(document).ready(function(){
           disableOnInteraction: false,
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.next',
+          prevEl: '.prev',
         },
       });
     } else {
@@ -40,8 +40,8 @@ $(document).ready(function(){
           disableOnInteraction: false,
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.next',
+          prevEl: '.prev',
         },
       });
     }
@@ -87,9 +87,53 @@ $(document).ready(function(){
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       }
     ]
   });
+
+  $(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }]
+  });
+  
+  
+  
+});
+
+
+
+$(document).mouseup(function(e) {
+  var container = $("#collapseExample");
+
+  // If the target of the click isn't the container nor a descendant of the container
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.collapse('hide');
+  }
 });
